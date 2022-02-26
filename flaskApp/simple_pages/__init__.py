1b6e2ae1e629c728831186acd943e9aed8ca1630
sagedemage <salsaeed999@gmail.com>
2022-02-26 01:05:38 -0500

    Adding simple web pages

---
+++ flaskApp/simple_pages/__init__.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, render_template, abort, Flask
+
+from jinja2 import TemplateNotFound
+
+
+bp = Blueprint('simple_pages', __name__,
+                        template_folder='templates',static_folder='static')
+
+@bp.route('/page/<page>')
+def show(page):
+    try:
+        return render_template('%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
